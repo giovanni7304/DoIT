2599f426705b860c55121a1d5856a5a89151b12b
Terry Johnson <giovanni7304@me.com>
2016-09-30 10:51:44 -0500

    Loaded the cells with data

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Terry Johnson on 9/30/16.
+//  Copyright © 2016 Terry Johnson. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name : String = ""
+    var important : Bool = false
+}
